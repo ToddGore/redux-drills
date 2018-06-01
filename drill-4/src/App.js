60e250f81ef17b9f4e1b8297a4fb64e94077435b
Todd Gore <toddgore@Todds-MacBook-Pro.local>
2018-06-01 08:59:33 -0600

    Initial commit

--- drill-4/src/App.js
+++ drill-4/src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import { addGuest, removeGuest } from './ducks/guestList';
+import { addGuest, removeGuest, updateName } from './ducks/guestList';
+import EditGuest from './components/EditGuest/EditGuest';
 import { connect } from 'react-redux';
 import './App.css';
-
-
 
 class App extends Component {
   constructor() {
@@ -12,20 +11,14 @@ class App extends Component {
       text: '',
       edit: false,
       guestToEdit: '',
-      guestIndex: 0
+      index: 0
     }
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.editName = this.editName.bind(this);
+    this.showModal = this.showModal.bind(this);
     this.hideModal = this.hideModal.bind(this);
-  }
-
-  editName(guest, i) {
-    this.setState({
-      edit: true,
-      guestToEdit: guest,
-      guestIndex: i
-    })
+    this.editName = this.editName.bind(this);
+    this.updateGuestName = this.updateGuestName.bind(this)
   }
 
   handleInputChange(e) {
@@ -41,10 +34,29 @@ class App extends Component {
     })
   }
 
+  showModal(guest, index) {
+    this.setState({
+      edit: true,
+      guestToEdit: guest,
+      index: index
+    })
+  }
+
   hideModal() {
     this.setState({
       edit: false
     })
+  }
+
+  editName(e) {
+    this.setState({
+      guestToEdit: e.target.value
+    })
+  }
+
+  updateGuestName() {
+    this.props.updateName(this.state.guestToEdit, this.state.index)
+    this.hideModal()
   }
 
   render() {
@@ -53,39 +65,41 @@ class App extends Component {
         <h1>DevMountain Hackathon</h1>
         <h3>Guest List:</h3>
         <ul>
-          {this.props.list.map( (guest, i) => {
-            return (
-              <div key={i} className="list-item">
-                <li>{guest}</li>
-                <div className="">
-                  <button
-                    onClick={() => this.editName(guest, i)}
-                    type="">Edit</button>
-                  <button
-                    onClick={()=> this.props.removeGuest(i)}
-                    type=""
-                    className="">Remove</button>
+          {
+            this.props.list.map((guest, i) => {
+              return (
+                <div key={i} className="list-item">
+                  <li>{guest}</li>
+                  <div>
+                    <button
+                      onClick={() => this.showModal(guest, i)}
+                    >Edit</button>
+                    <button onClick={() => this.props.removeGuest(i)}>Remove</button>
+                  </div>
                 </div>
-              </div>
-            )
-          })}
+              )
+            })
+          }
         </ul>
         <form
           onSubmit={this.handleSubmit}
           className="add-guest">
           Add guest: <input
-          value={this.state.text}
-          onChange={this.handleInputChange}
-          type="" className=""/>
-          <button
-            type=""
-            className="">Add</button>
+            value={this.state.text}
+            onChange={this.handleInputChange}
+          />
+          <button>Add</button>
         </form>
         {
-           this.state.edit ?
-                {/* EditGuest */}
-                : null
+          this.state.edit ?
+            <EditGuest
+              hide={this.hideModal}
+              guest={this.state.guestToEdit}
+              edit={this.editName}
+              update={this.updateGuestName} />
+            : null
         }
+
       </div>
     );
   }
@@ -97,4 +111,4 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps,{ addGuest, removeGuest })(App);
+export default connect(mapStateToProps, { addGuest, removeGuest, updateName })(App);
